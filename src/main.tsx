71a675bd64911eed2e2935091c08ef821e12d015
johnn <ikg1366@ya.ru>
2025-03-18 20:12:57 +0300

    PR fix 4.1

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './app/styles/index.scss'
 import App from './app/App.tsx'
 import '@ant-design/v5-patch-for-react-19';
-import {Provider} from "react-redux";
-import {store} from "@/app/providers/StoreProvoder/config/strore.ts";
+// import {Provider} from "react-redux";
+// import {store} from "@/app/providers/StoreProvoder/config/strore.ts";
 
 createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
+    // <Provider store={store}>
         <StrictMode>
                 <App />
         </StrictMode>,
-    </Provider>
+    // </Provider>
 
 )

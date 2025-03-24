4dd9e7afe694d0e2a561fce81a1784f9d88f9896
johnn <ikg1366@ya.ru>
2025-03-25 01:37:39 +0300

    tests bags fix

--- src/pages/UserPage/ui/UserPage.tsx
+++ src/pages/UserPage/ui/UserPage.tsx
@@ -1,9 +1,34 @@
+import {useDispatch, useSelector} from "react-redux";
+import {StateSchema} from "@/app/providers/StoreProvoder/config/StateSchema.ts";
+import {Button} from "antd";
+import {tokenService} from "@/features/Authentication/service/TokenService.ts";
+import {userApi} from "@/entities/User/api/userApi.ts";
+import {AppDispatch} from "@/app/providers/StoreProvoder/config/store.ts";
 
 
 export const UserPage = () => {
+    const [logout, {isLoading}] = userApi.useLogoutMutation()
+    const dispatch = useDispatch<AppDispatch>()
+    const handleLogout = async () => {
+        try {
+            await logout(undefined)
+            tokenService.logoutOnClient(dispatch)
+        } catch (error){
+            console.error(error)
+        }
+
+
+    }
+    const user = useSelector((state: StateSchema) => state.auth.User);
     return (
         <div>
-            привет
+            <h3>{user?.username}</h3>
+            <h3>{user?.email}</h3>
+            <h3>{user?.phoneNumber}</h3>
+            <Button
+                onClick={handleLogout}
+                loading={isLoading}
+            >Logout</Button>
         </div>
     );
 };
--- src/widgets/Sidebar/ui/MainMenu.tsx
+++ src/widgets/Sidebar/ui/MainMenu.tsx
@@ -1,16 +1,12 @@
 import {Link} from "react-router-dom";
 import cls from './Sidebar.module.scss'
-import {Button, Menu} from "antd";
+import {Menu} from "antd";
 import Sider from "antd/es/layout/Sider";
-import {tokenService} from "@/features/Authentication/service/TokenService.ts";
-import {userApi} from "@/entities/User/api/userApi.ts";
-import {useDispatch} from "react-redux";
-import {AppDispatch} from "@/app/providers/StoreProvoder/config/store.ts";
+
 
 
 export const MainMenu = () => {
-    const [logout, {isLoading}] = userApi.useLogoutMutation()
-    const dispatch = useDispatch<AppDispatch>()
+
     enum PagePath {
         MAIN = '/',
         USER = '/user'
@@ -21,16 +17,7 @@ export const MainMenu = () => {
         { path: PagePath.USER, name: 'Профиль' },
 
     ];
-    const handleLogout = async () => {
-        try {
-            await logout(undefined)
-            tokenService.logoutOnClient(dispatch)
-        } catch (error){
-            console.error(error)
-        }
 
-
-    }
 
     return (
         <Sider className={cls.sidebar}>
@@ -42,10 +29,7 @@ export const MainMenu = () => {
                       )
                   }))}
             />
-            <Button
-                onClick={handleLogout}
-                loading={isLoading}
-            >Logout</Button>
+
         </Sider>
     );
 };
